64e15f7b1e51170c38fcf2ac5cf06a7abd45dfbd
annascodes <itsanascode@gmail.com>
2025-08-29 01:02:14 +0500

    login bug resolved

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -8,9 +8,7 @@ import AuditLog from "@/lib/models/auditLogModel"
 
 export async function POST(req) {
     const body = await req.json()
-    if (!body?.email || body.email.trim() === '' || !body?.email.includes('@')) {
-        return NextResponse.json({ error: 'Enter valid email!!!' }, { status: 409 })
-    }
+ 
     if (!body?.username || body.username.trim() === '') {
         return NextResponse.json({ error: 'Enter valid username!!!' }, { status: 409 })
     }
